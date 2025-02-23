a1db4bc4cd53a49d85a381323330fdce54a76fc7
omgaXD <sladekop@gmail.com>
2025-02-23 14:24:57 +0500

    First Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import { heroui } from "@heroui/react";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",    
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,6 +15,7 @@ export default {
         foreground: "var(--foreground)",
       },
     },
-  },
-  plugins: [],
+  }, 
+  darkMode: "class",
+  plugins: [heroui()],
 } satisfies Config;
